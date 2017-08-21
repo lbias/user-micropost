e5093c10e1bb7093dbabbff1a992647f198000cc
lbias <306508354@qq.com>
2017-08-21 14:05:30 +0800

    add password_digest to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170821054923) do
+ActiveRecord::Schema.define(version: 20170821055959) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170821054923) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "authentication_token"
+    t.string "password_digest"
   end
 
 end
