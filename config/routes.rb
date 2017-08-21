215d42a3b84fb157d1947722e4c29d9fb95b8361
lbias <306508354@qq.com>
2017-08-21 16:15:40 +0800

    controller api::v2::users#show

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,15 @@ Rails.application.routes.draw do
       resources :sessions, only: [:create]
       scope path: '/user/:user_id' do
         resources :microposts, only: [:index]
-      end      
+      end
     end
+
+    namespace :v2 do
+      resources :users, only: [:index, :create, :show, :update, :destroy]
+      resources :sessions, only: [:create]
+      scope path: '/user/:user_id' do
+        resources :microposts, only: [:index]
+      end
+    end    
   end
 end
