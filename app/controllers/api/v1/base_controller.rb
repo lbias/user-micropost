b358ba466aafb5b47c3ed53d37048cfb33a471cd
lbias <306508354@qq.com>
2017-08-21 14:28:13 +0800

    view api::v1::sessions#create.json.jbuilder

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -5,7 +5,13 @@ class Api::V1::BaseController < ApplicationController
   # disable cookies (no set-cookies header in response)
   before_action :destroy_session
 
+  attr_accessor :current_user
+
   def destroy_session
     request.session_options[:skip] = true
   end
+
+  def api_error(opts = {})
+    render nothing: true, status: opts[:status]
+  end  
 end
