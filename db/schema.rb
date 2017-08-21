bed88bb2fefcfe0fd0ca52fb0f3550303de779d0
lbias <306508354@qq.com>
2017-08-21 13:51:50 +0800

    add authentication_token to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170821051442) do
+ActiveRecord::Schema.define(version: 20170821054923) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170821051442) do
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "authentication_token"
   end
 
 end
